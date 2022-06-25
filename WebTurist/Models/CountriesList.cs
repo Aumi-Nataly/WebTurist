2a7f7cd82183e060a237451260a9c794ebf51d60
Nataly <mvnataly@yandex.ru>
2022-06-25 17:42:34 +0300

    верстка главной страницы и шапки

---
+++ WebTurist/Models/CountriesList.cs
@@ -0,0 +1,11 @@
+﻿namespace WebTurist.Models
+{
+    /// <summary>
+    /// Список всех стран
+    /// </summary>
+    public class CountriesList
+    {
+        public int countryId { get; set; }
+        public string countryName { get; set; }
+    }
+}
--- WebTurist/Pages/Index.cshtml.cs
+++ WebTurist/Pages/Index.cshtml.cs
@@ -1,14 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebTurist.Models;
 
 namespace WebTurist.Pages
 {
     public class IndexModel : PageModel
     {
-      
+        public readonly List<CountriesList> countrylist;
         public IndexModel()
         {
-        
+            countrylist=new List<CountriesList>();
+            countrylist.Add(new CountriesList { countryId = 1, countryName = "Россия" });
+            countrylist.Add(new CountriesList { countryId = 2, countryName = "Греция" });
         }
 
         public void OnGet()
